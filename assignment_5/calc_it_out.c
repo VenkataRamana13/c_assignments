06bd82ec9062ed7f54f510d777d771e4be2f935c
Ramana <ramana.allamraju@gmail.com>
2022-02-25 11:28:58 +0530

    calc_it_out solved

---
+++ assignment_5/calc_it_out.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+long long MOD = 1e9 + 7; 
+
+int main(){
+    int N, M; scanf("%d%d", &N, &M); 
+    int a[N][M]; 
+    for (int *p = (int *)a; p <= &a[N - 1][M - 1]; p++){
+        scanf ("%d", p); 
+        }
+    int Q; scanf("%d", &Q); 
+    int b[Q][4]; 
+    for (int *p = (int *)b; p <= &b[Q-1][3]; p++){
+        scanf("%d", p); 
+    }
+    for (int i = 0; i < Q; i++){
+        long long product = 1; 
+        for (int x = b[i][0]; x <= b[i][2]; x++){
+            for (int y = b[i][1]; y <= b[i][3]; y++){
+                product = (long long)(product*a[y][x]%MOD);
+            }
+        }
+        printf("%lld\n", product);
+    }
+}
