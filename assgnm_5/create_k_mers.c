5def00895f2cf42e220e6aae84107cf774e9c94f
Ramana <ramana.allamraju@gmail.com>
2022-03-05 16:09:32 +0530

    calc_it_out modified

--- assgnm_5/create_k_mers.c
+++ assgnm_5/create_k_mers.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 int main(){
     int L, K, m; scanf("%d%d%d", &L, &K, &m); 
-    char s[L+1]; gets(s);  
-    for (int i = 0; i < L; i += K - m){ 
-        for (int j = 0; j < K; j++){
-            if (i + j > L){
-
-            printf ("%c", s[i + j]); 
+    char s[L+1]; scanf("%s", s); 
+    for (int i = 0; i < L;  i++){
+        if (!(i % K)){
+            printf("\n"); 
+            i -= m;   
+            for (int j = 0; j < K; j++){
+                printf("%c", s[i]);
+                if (i == L-1) break; 
+                i++; 
+            }
+            printf("\n"); 
+        }
+        if (i == L-1) break; 
+        printf("%c", s[i]); 
     }
 }
+
